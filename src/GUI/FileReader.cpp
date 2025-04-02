403949a4e18329d0dce7a37ddc9f56222cf7c43d
wzyle <lequocvi2412@gmail.com>
2025-04-02 10:44:43 +0700

    Add tinyfiledialogs

--- src/GUI/FileReader.cpp
+++ src/GUI/FileReader.cpp
@@ -1,60 +1,26 @@
 #include "GUI/FileReader.hpp"
-
-#ifdef _WIN32  // Windows
-#include <windows.h>
 #include <iostream>
+#include <string>
+#include <codecvt>
+#include <locale>
+#include "GUI/tinyfiledialogs.h"
 
 std::wstring OpenFileDialog() {
-    wchar_t originalDir[MAX_PATH];
-    GetCurrentDirectoryW(MAX_PATH, originalDir);
+    // Use tinyfiledialogs for all platforms
+    const char* filePath = tinyfd_openFileDialog(
+        "Open File",           // Title
+        "",                    // Default path
+        0,                     // Number of filter patterns
+        nullptr,               // Filter patterns (nullptr for all files)
+        nullptr,               // Single filter description
+        0                      // Allow multiple selects (0 = no)
+    );
 
-    OPENFILENAMEW ofn;
-    wchar_t szFile[MAX_PATH] = {0};
-    ZeroMemory(&ofn, sizeof(ofn));
-
-    ofn.lStructSize = sizeof(ofn);
-    ofn.hwndOwner = NULL;
-    ofn.lpstrFilter = L"All Files\0*.*\0";
-    ofn.lpstrFile = szFile;
-    ofn.nMaxFile = MAX_PATH;
-    ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
-
-    if (GetOpenFileNameW(&ofn)) {
-        std::wstring selectedFile(szFile);
-        SetCurrentDirectoryW(originalDir);
-        return selectedFile;
-    }
-
-    SetCurrentDirectoryW(originalDir);
-    return L"";
-}
-
-#elif __APPLE__  // macOS
-#include <iostream>
-#import <AppKit/AppKit.h>
-
-std::wstring OpenFileDialog() {
-    @autoreleasepool {
-        NSOpenPanel* openPanel = [NSOpenPanel openPanel];
-        [openPanel setCanChooseFiles:YES];
-        [openPanel setCanChooseDirectories:NO];
-        [openPanel setAllowsMultipleSelection:NO];
-
-        if ([openPanel runModal] == NSModalResponseOK) {
-            NSURL* selectedFileURL = [[openPanel URLs] firstObject];
-            if (selectedFileURL) {
-                std::string path = std::string([[selectedFileURL path] UTF8String]);
-                return std::wstring(path.begin(), path.end());
-            }
-        }
+    if (filePath) {
+        // Convert UTF-8 char* to wstring
+        std::string utf8Path(filePath);
+        std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+        return converter.from_bytes(utf8Path);
     }
     return L"";
 }
-
-#else  // Unsupported OS
-#include <iostream>
-std::wstring OpenFileDialog() {
-    std::cerr << "OpenFileDialog is not supported on this platform." << std::endl;
-    return L"";
-}
-#endif
